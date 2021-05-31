35076e4bbc5b2bb702523cd5d2e71b0eeb6dd194
Muhammad Jawad <jawad.topdot@gmail.com>
2021-05-31 16:41:36 +0500

    added provider

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Topdot\Admin;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+use Livewire;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    public $routeFilePath = '/routes/laravel-admin.php';
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(realpath(__DIR__.'/resources/views/'), 'laravel-admin');
+        $this->mergeConfigFrom(__DIR__.'/config.php', 'laravel-admin');
+
+        $this->setupRoutes($this->app->router);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishFiles();            
+        }
+    }
+
+
+
+    /**
+     * Define the routes for the application.
+     *
+     * @param \Illuminate\Routing\Router $router
+     *
+     * @return void
+     */
+    public function setupRoutes(Router $router)
+    {
+        // by default, use the routes file provided in vendor
+        $routeFilePathInUse = __DIR__.$this->routeFilePath;
+
+        // but if there's a file with the same name in routes/backpack, use that one
+        if (file_exists(base_path().$this->routeFilePath)) {
+            $routeFilePathInUse = base_path().$this->routeFilePath;
+        }
+
+        $this->loadRoutesFrom($routeFilePathInUse);
+    }
+
+    public function publishFiles()
+    {
+        $this->publishes([
+            __DIR__.'/config.php' => config_path('laravel-admin.php'),
+        ], 'config');
+    }
+}
