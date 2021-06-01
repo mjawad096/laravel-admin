9aa9a0c2b0b39611972f4fc04c4bc6a0d2b53aba
Muhammad Jawad <mjawad096@gmail.com>
2021-06-01 14:45:48 +0500

    custom route file changed

--- src/routes/laravel-admin/custom.php
+++ src/routes/laravel-admin/custom.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->name('laravel-admin.')->group(function(){
+Route::group([
+    'namespace'  => 'App\Http\Controllers',
+    'middleware' => config('laravel-admin.router.middleware_web', 'web'),
+    'prefix'     => config('laravel-admin.router.prefix', 'admin'),
+    'as'     => 'laravel-admin.',
+], function(){
 	
 });
