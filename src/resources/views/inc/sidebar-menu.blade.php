f155c612e0b59c587fe4b8ebba8ceb0714742b20
Muhammad Jawad <mjawad096@gmail.com>
2021-06-01 09:54:48 +0500

    added some configurations created sidebar menu blade

---
+++ src/resources/views/inc/sidebar-menu.blade.php
@@ -0,0 +1,8 @@
+<li class="navigation-header"><span>Example</span></li>
+
+<li class="nav-item {{ $menu_active == 'example' ? 'active' : ''}}">
+    <a href="/example">
+        <i class="feather icon-archive"></i>
+        <span class="menu-title">Example</span>
+    </a>
+</li>
--- src/resources/views/inc/sidebar.blade.php
+++ src/resources/views/inc/sidebar.blade.php
@@ -2,13 +2,18 @@
 <div class="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
     <div class="navbar-header">
         <ul class="nav navbar-nav flex-row">
-            <li class="nav-item mr-auto"><a class="navbar-brand" href="/">
+            <li class="nav-item mr-auto"><a class="navbar-brand" href="{{ config('laravel-admin.dashboard_url', '/') }}">
                     <div class="brand-logo"></div>
-                    <h2 class="brand-text mb-0">Laravel Admin</h2>
+                    <h2 class="brand-text mb-0">{{ config('laravel-admin.sidebar.main_heading', 'Laravel Admin') }}</h2>
                 </a></li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i></a></li>
         </ul>
     </div>
     <div class="shadow-bottom"></div>
+    <div class="main-menu-content">
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+            @include('laravel-admin::inc.sidebar-menu')            
+        </ul>
+    </div>
 </div>
 <!-- END: Main Menu-->
