a161d446d9136ec645cdff5abd1dd5ea968dc554
Muhammad Jawad <mjawad096@gmail.com>
2021-08-05 15:01:54 +0500

    details page added

---
+++ src/resources/views/crud/details.blade.php
@@ -0,0 +1,29 @@
+@extends('laravel-admin::layouts.app')
+
+@section('content')
+    <section>
+        <div class="row match-height">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{ $entery }} detials</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body">
+                            <table class="table table-striped mb-0">
+                                <tbody>
+                                    @foreach ($item->toArray() as $key => $value)
+                                        <tr>
+                                            <td><strong>{{ title_case($key) }}</strong></td>
+                                            <td>{{ toString($value) }}</td>
+                                        </tr>                                
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
