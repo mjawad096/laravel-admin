efc94a23ae5f5dcc1477b0d2e441a21d093e422a
Muhammad Jawad <jawad.topdot@gmail.com>
2021-09-23 16:51:02 +0500

    Added relativeInclude directive

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -55,6 +55,22 @@ class AdminServiceProvider extends ServiceProvider
             $this->publishFiles();            
         }
 
+        Blade::directive('relativeInclude', function ($args) {
+            $args = Blade::stripParentheses($args);
+            
+            $viewBasePath = Blade::getPath();
+            foreach ($this->app['config']['view.paths'] as $path) {
+                if (substr($viewBasePath,0,strlen($path)) === $path) {
+                    $viewBasePath = substr($viewBasePath,strlen($path));
+                    break;
+                }
+            }
+    
+            $viewBasePath = dirname(trim($viewBasePath,'\/'));
+            $args = substr_replace($args, $viewBasePath.'.', 1, 0);
+            return "<?php echo \$__env->make({$args}, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>";
+        });
+
         Blade::directive('pushonce', function ($expression) {
             $var = '$__env->{"__pushonce_" . md5(__FILE__ . ":" . __LINE__)}';
 
