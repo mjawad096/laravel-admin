e6c3208719bb992dde9c7805af37e3c78601033a
Muhammad Jawad <mjawad096@gmail.com>
2021-12-27 13:08:23 +0500

    On deleting a file will not delete it from DB.

--- src/resources/views/fields/file.blade.php
+++ src/resources/views/fields/file.blade.php
@@ -1,4 +1,9 @@
-<livewire:media::temp-file-upload-component :name="$name" :config="['accept' => ($accept ?? ($type == 'image' ? 'image/*' : '*'))]" :maxFiles="$maxFiles ?? 10" :totalFiles="$editing_form ? $item->getMedia($name)->count() : null"/>
+@php
+    $total_files = $editing_form ? $item->getMedia($name)->count() - count(old("deleted_files.{$name}", [])) : null;
+@endphp
+
+<livewire:media::temp-file-upload-component :name="$name" :config="['accept' => ($accept ?? ($type == 'image' ? 'image/*' : '*'))]" :maxFiles="$maxFiles ?? 10" :totalFiles="$total_files"/>
+
 @if($editing_form)
     <livewire:media::file-preview-component :model="$item" :collection="$name" />
 @endif
